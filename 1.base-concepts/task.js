199091ad6f0c73802cc4d0a78a977ead006399ae
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2022-12-23 13:03:54 +0300

    Сделано 2 задания
    Сделаны 2 задания. Внесены правки по кодстайлу

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
 "use strict"
+
+// Task #1
 function solveEquation(a, b, c) {
-  let arr = [];
+
+  let d = Math.pow(b, 2) - 4 * a * c;
+  let res = [];
   
-  return arr;
+  if(d < 0) {
+    return res;
+  } else if (d === 0) {
+    res.push(-b/(2 * a));
+  } else if (d > 0) {
+    res.push( (-b + Math.sqrt(d) )/(2 * a) ); 
+    res.push( (-b - Math.sqrt(d) )/(2 * a) );
+  }
+  return res;
 }
 
+// Task #2
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  if(isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  } 
+  if(countMonths < 0 || amount < 0) {
+    return false;
+  } 
+
+  let p = percent / 100 / 12;
+  let body = amount - contribution;
+  let monthlyPayment = body * (p + (p / ((Math.pow((p + 1), countMonths) - 1))));
+  let overallPayment = Number((monthlyPayment * countMonths).toFixed(2))
+  return overallPayment
 }
