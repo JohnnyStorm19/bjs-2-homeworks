70dd734e53ef44c61293327667ac27af69106548
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-01-06 16:58:14 +0300

    Выполнено задание по блоку

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+let student1 = new Student();
+let student2 = new Student();
+let student3 = new Student();
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty('marks')) {
+    (this.marks).push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(!this.hasOwnProperty('marks') || !(this.marks).length) {
+    return 0;
+  } else {
+    return ( (this.marks).reduce( (acc, current) => acc + current) ) / (this.marks).length;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete(this.subject);
+  delete(this.marks);
+  this.excluded = reason;
 }
