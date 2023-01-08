eb2b4a896a915cc1b4dd9fc4b6147a5ec58989fb
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-01-08 16:48:32 +0300

    Внесены исправления
    Внес небольшие исправления в код, добавил примеры

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,30 +5,38 @@ function Student(name, gender, age) {
   this.marks = [];
 }
 
-let student1 = new Student();
-let student2 = new Student();
-let student3 = new Student();
-
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
   if (this.hasOwnProperty('marks')) {
-    (this.marks).push(...marks);
+    this.marks.push(...marks);
   }
 }
 
 Student.prototype.getAverage = function () {
-  if(!this.hasOwnProperty('marks') || !(this.marks).length) {
-    return 0;
+  if (this.hasOwnProperty('marks') && (this.marks).length > 0){
+    return (this.marks.reduce( (acc, current) => acc + current) ) / (this.marks).length;
   } else {
-    return ( (this.marks).reduce( (acc, current) => acc + current) ) / (this.marks).length;
+    return 0;
   }
 }
 
 Student.prototype.exclude = function (reason) {
-  delete(this.subject);
-  delete(this.marks);
+  delete this.subject;
+  delete this.marks;
   this.excluded = reason;
 }
+
+let student1 = new Student("Гарри", "мужской", 11);
+student1.setSubject("Quidduch");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+
+let student2 = new Student("Том", "мужской", 16);
+student2.setSubject("Forbidden magic");
+student2.exclude("Занимался черной магией");
+console.log(student2);
