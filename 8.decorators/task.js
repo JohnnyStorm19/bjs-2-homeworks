05b174d56f34519957bac62743afc124bf82122f
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-01-31 00:04:48 +0300

    Выполнены задания по 8 блоку

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,44 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+    function wrapper(...args) {
+        const hash = md5(args);
+        const objectInCache = cache.find(item => item.hash === hash);
+        if (objectInCache) {
+            console.log('Из кэша: ' + objectInCache.value);
+            return 'Из кэша: ' + objectInCache.value;
+        }
+        let result = func(...args);
+        cache.push({ hash: hash, value: result });
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log('Вычисляем: ' + result);
+        return 'Вычисляем: ' + result;
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    function wrapper(...args) {
+        wrapper.allCount++;
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        if (timeoutId === null) {
+            func(...args);
+            wrapper.count++;
+        }
+        timeoutId = setTimeout(() => {
+            wrapper.count++;
+            func(...args);
+        }, delay);
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
 }
+
+
